8c14432ab9a6e8459af867fa4ca78746862a7338
Дмитрий Передрий <102544753+Japrojah@users.noreply.github.com>
2023-02-18 22:43:53 +0300

    100% done

--- yatube_api/yatube_api/urls.py
+++ yatube_api/yatube_api/urls.py
@@ -11,4 +11,3 @@ urlpatterns = [
         name='redoc'
     ),
 ]
-#eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjc3MjczODM1LCJqdGkiOiI2NGQ4NGUxYzVhYWQ0ZDk3OGIwOWI3NTQ3MTVjZDdkZSIsInVzZXJfaWQiOjF9.ngLq91bVadysCbnEgs53prcU9pAWj_tgdqw-3x3x-Ko
